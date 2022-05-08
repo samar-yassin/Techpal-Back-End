314d87b03d40f904a3b556b829612f960666aa0d
Marwa Omar <farabi.marwa@gmail.com>
2022-05-09 01:25:24 +0200

    Update router to include gettrack enpoint

--- routes/adminRouter.go
+++ routes/adminRouter.go
@@ -8,6 +8,7 @@ import (
 
 func AdminRoutes(incomingRoutes *gin.Engine) {
 	incomingRoutes.POST("api/addTrack", controller.AddTrack())
+	incomingRoutes.GET("api/gettrack/:track_id", controller.GetTrack())
 	incomingRoutes.GET("api/getalltracks", controller.GetAllTracks())
 	incomingRoutes.POST("api/acceptmentor", controller.AcceptMentor())
 	incomingRoutes.POST("api/reportmentor", controller.ReportMentor())
