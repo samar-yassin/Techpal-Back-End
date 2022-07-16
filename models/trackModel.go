830254f2c9dc95556f5f4c26891d13b7c0c295e9
Samar Yassin <samareeyassin155@gmail.com>
2022-07-17 01:21:12 +0200

    skill unit model

--- models/trackModel.go
+++ models/trackModel.go
@@ -5,8 +5,8 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 type Track struct {
 	ID       primitive.ObjectID `bson:"_id"`
 	Track_id string
-	Name     *string        `json:"name" validate:"required,min=2,max=100"`
-	Color1   *string        `json:"color1" validate:"required"`
-	Color2   *string        `json:"color2" validate:"required"`
-	Skills   map[string]int `json:"skills" validate:"required"`
+	Name     *string              `json:"name" validate:"required,min=2,max=100"`
+	Color1   *string              `json:"color1" validate:"required"`
+	Color2   *string              `json:"color2" validate:"required"`
+	Skills   map[string]skillUnit `json:"skills" validate:"required"`
 }
